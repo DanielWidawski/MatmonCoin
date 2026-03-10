c2beef7e2fbeffb6603fb5aad42713f12ad528dd
Jonathan <jonathanfeldman18@gmail.com>
2026-03-10 22:09:44 +0200

    update kafka consumer to deal with msg schema

--- kafka_consumer.py
+++ kafka_consumer.py
@@ -2,31 +2,50 @@ import json
 import sys
 from json import JSONDecodeError
 
-import pika
+# import pika
 
 from confluent_kafka import Consumer, KafkaException
+
+
+def check_message_schema(message: dict) -> bool:
+    if message.get("market") and message.get("message"):
+        if type(message["message"]) == str:
+            message = json.loads(message["message"])
+        return message["message"].get("data")
+    return False
+
+
+def get_original_message(message):
+    message = message.get('message')
+    if type(message.get('message')) == str:
+        message = json.loads(message.get('message'))
+    return message
+
+
+def get_original_data(message):
+    date = message.get("data")
+    if type(date) == str:
+        date = json.loads(date)
+    return date
 
 
 def transform(message):
     new_schema = {}
     # message = {'market': 'Binance', 'message': '{"stream":"solusdt@trade","data":{"e":"trade","E":1773087732411,"T":1773087732411,"s":"SOLUSDT","t":3216441787,"p":"85.7800","q":"10.71","X":"MARKET","m":false}}'}
     # message2 = {'market': 'Binance', 'message': {'stream': 'btcusdt@aggTrade', 'data': {'e': 'aggTrade', 'E': 1773136021199, 'a': 3190727436, 's': 'BTCUSDT', 'p': '70974.50', 'q': '0.002', 'nq': '0.002', 'f': 7413816280, 'l': 7413816280, 'T': 1773136021194, 'm': False}}}
-    if message['market'] == 'Binance':
-        new_schema['Market'] = message.get("market")
-        if type(message.get('message')) == str:
-            message = json.loads(message.get('message'))
-        else:
-            message = message.get('message')
-        data = message.get("data")
-        #print(data.get("data"))
-        #print(new_schema)
-        new_schema['symbol'] = data.get("s")
-        new_schema['side'] = "sell" if data.get('m') else "buy"
-        new_schema['price'] = data.get("p")
-        new_schema['amount'] = data.get("q")
-        new_schema['timestamp'] = data.get("T")
-        print(new_schema)
-    return new_schema
+    if check_message_schema(message):
+        if message.get("market") == 'Binance':
+            new_schema['market'] = message.get("market")
+            message = get_original_message(message)
+            data = get_original_data(message)
+            # print(new_schema)
+            new_schema['symbol'] = data.get("s")
+            new_schema['side'] = "sell" if data.get('m') else "buy"
+            new_schema['price'] = data.get("p")
+            new_schema['amount'] = data.get("q")
+            new_schema['timestamp'] = data.get("T")
+        return new_schema
+    return None
 
 
 """def connect_to_mq():
@@ -45,7 +64,6 @@ def write_to_mq(msg):
                           body=msg)
     connection.close()"""
 
-
 conf = {
     'bootstrap.servers': 'localhost:9092',
     'group.id': 'my-consumer-group',
@@ -58,7 +76,7 @@ consumer = Consumer(conf)
 topic = 'my-topic'
 consumer.subscribe([topic])
 
-#connection, channel = connect_to_mq()
+# connection, channel = connect_to_mq()
 
 try:
     while True:
@@ -69,12 +87,11 @@ try:
             print(msg.error())
         else:
             try:
-                print(json.loads(msg.value().decode('utf-8')))
                 new_msg = json.loads(msg.value().decode('utf-8'))
                 transform_temp = transform(new_msg)
                 print(transform_temp)
-                #write_to_mq(channel, transform_temp)
-                #write_to_mq(transform)
+                # write_to_mq(channel, transform_temp)
+                # write_to_mq(transform)
                 consumer.commit(msg)
             except JSONDecodeError:
                 consumer.commit(msg)
@@ -83,7 +100,7 @@ except KeyboardInterrupt:
     sys.stderr.write('%% Aborted by user\n')
 finally:
     consumer.close()
-    #connection.close()
+    # connection.close()
 
 """import json
 import sys
@@ -121,4 +138,3 @@ except KeyboardInterrupt as e:
     sys.stderr.write('%% Aborted by user\n')
 finally:
     consumer.close()"""
-
