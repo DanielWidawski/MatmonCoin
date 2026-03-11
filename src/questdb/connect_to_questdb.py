dd0ec83af8986f7b16fb68bee7565b2d3c8bbdfc
ofekax <mtmn16@matmon-development-machine.xmeg0jdevaee3lh03vzpt13orh.tlvx.internal.cloudapp.net>
2026-03-11 15:55:38 +0000

    create and make sampled_by_minutes_trades_binance table with deduplication - UPDATE VERSION

--- src/questdb/connect_to_questdb.py
+++ src/questdb/connect_to_questdb.py
@@ -2,27 +2,39 @@
 import psycopg as pg
 import time
 # Connect to an existing QuestDB instance
-conn_str = 'user=admin password=quest host=0.0.0.0 port=8812 dbname=trades-BINANCE'
+conn_str = 'user=admin password=quest host=0.0.0.0 port=8812 dbname=trades-BINANCE'    
+
+def create_sampled_trades_by_minute_binance_table() -> None:
+    cur.execute("""CREATE TABLE sampled_trades_by_minute_binance  (
+                symbol symbol,
+                ts timestamp,
+                avg_price double
+                ) timestamp(ts) PARTITION BY DAY WAL
+                DEDUP UPSERT KEYS(symbol, ts);
+                    """)
+
+def insert_data_into_sampled_trades_by_minute_binance_table() -> None:
+    cur.execute(""" INSERT INTO sampled_trades_by_minute_binance  
+                SELECT symbol, timestamp, avg(price)
+                FROM "trades-BINANCE"
+                SAMPLE BY 1m;         
+                    """)
+    
+
+
+
 with pg.connect(conn_str, autocommit=True) as connection:
     # Open a cursor to perform database operations
     with connection.cursor() as cur:
         #Query the database and obtain data as Python objects.
-         cur.execute("""CREATE TABLE sampled_trades_by_minute_binance (
-                    symbol symbol,
-                    ts timestamp,
-                    avg_price double) timestamp(ts) PARTITION BY DAY TTL 7 DAYS;
-                     
-                    ALTER TABLE sampled_trades_by_minute_binance DEDUP DISABLE;
-                     
-                    INSERT INTO "sampled_trades_by_minute_binance"
-                    SELECT symbol, timestamp, avg(price)
-                    FROM "trades-BINANCE"
-                    SAMPLE BY 1m
-                    FILL(NULL, NULL, NULL);  
-                     
-                    
-    
+        insert_data_into_sampled_trades_by_minute_binance_table()
+        cur.execute("""SELECT * FROM sampled_trades_by_minute_binance  
                     """)
+        records = cur.fetchall()
+        for row in records:
+            print(row)    
+         
+  
                 
 
         
