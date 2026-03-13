c496fa23a9ea2e46557221d96d8fac0cc7b2d88e
ofekax <mtmn16@matmon-development-machine.xmeg0jdevaee3lh03vzpt13orh.tlvx.internal.cloudapp.net>
2026-03-13 10:38:14 +0000

    create binance_trades_hourly materilized view

--- src/questdb/connect_to_questdb.py
+++ src/questdb/connect_to_questdb.py
@@ -2,12 +2,12 @@
 import psycopg as pg
 import time
 
-def create_sampled_trades_binance_table(smapled_table_name: str) -> None:
-    cur.execute(f"""CREATE TABLE {smapled_table_name}  (
+def create_sampled_trades_binance_materialized_view(smapled_table_name: str) -> None:
+    cur.execute(f"""CREATE MATERIALIZED VIEW {smapled_table_name}  (
                 symbol symbol,
                 ts timestamp,
                 avg_price double
-                ) timestamp(ts) PARTITION BY DAY WAL
+                ) timestamp(ts) PARTITION BY DAY TTL 7 DAYS
                 DEDUP UPSERT KEYS(symbol, ts);
                     """)
 
@@ -18,14 +18,27 @@ def insert_sampled_trades_data_into_a_table(sampled_by: str, table_name: str) ->
                 SAMPLE BY {sampled_by};     
                     """)
     
+def create_binance_trades_materilized_view(materilized_view_name: str, sample_by: str) -> None:
+   cur.execute(f""" CREATE MATERIALIZED VIEW {materilized_view_name} REFRESH IMMEDIATE AS (
+                    SELECT symbol, timestamp, avg(price) AS avg_price 
+                    FROM "trades"
+                    SAMPLE BY 1m 
+               ) PARTITION BY DAY TTL 7 DAYS
+                    """)
+   
+
 
 conn_str = 'user=admin password=quest host=0.0.0.0 port=8812 dbname=trades-BINANCE'    
 
-sampled_by_hour_table_name: str = 'sampled_trades_by_hour'
+sampled_by_hour_materilized_view_name: str = 'binance_trades_hourly'
 sampled_by_hour: str = '1h'
 
-sampled_by_minute_table_name: str = 'sampled_trades_by_minute'
+sampled_by_minute_materilized_view_name: str = 'sampled_trades_by_minute'
 sampled_by_minute: str = '1m'
+
+sampled_by_day_materilized_view_name: str = 'sampled_trades_by_day'
+sampled_by_day: str = '1d'
+
 
 
 with pg.connect(conn_str, autocommit=True) as connection:
@@ -33,9 +46,8 @@ with pg.connect(conn_str, autocommit=True) as connection:
     with connection.cursor() as cur:
         #Query the database and obtain data as Python objects.
        
-       #create_sampled_trades_binance_table(smapled_table_name=sampled_by_minute_table_name)
-       #insert_sampled_trades_data_into_a_table(sampled_by=sampled_by_minute, table_name=sampled_by_minute_table_name)
-       cur.execute(f"""SELECT * FROM {sampled_by_minute_table_name}  
+       #create_binance_trades_materilized_view(materilized_view_name=sampled_by_hour_materilized_view_name, sample_by=sampled_by_hour)
+       cur.execute(f"""SELECT * FROM {sampled_by_hour_materilized_view_name}  
                     """)
        records = cur.fetchall()
        for row in records:
