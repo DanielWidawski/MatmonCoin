1bd58d281cdc1d80a5525296cfc4506f6a111931
ofekax <mtmn16@matmon-development-machine.xmeg0jdevaee3lh03vzpt13orh.tlvx.internal.cloudapp.net>
2026-03-13 08:52:18 +0000

    added 1 hour interval

--- src/questdb/connect_to_questdb.py
+++ src/questdb/connect_to_questdb.py
@@ -2,9 +2,6 @@
 import psycopg as pg
 import time
 
-# Connect to an existing QuestDB instance
-
-
 def create_sampled_trades_binance_table(smapled_table_name: str) -> None:
     cur.execute(f"""CREATE TABLE {smapled_table_name}  (
                 symbol symbol,
@@ -14,11 +11,11 @@ def create_sampled_trades_binance_table(smapled_table_name: str) -> None:
                 DEDUP UPSERT KEYS(symbol, ts);
                     """)
 
-def insert_sampled_trades_by_minute_data_into_binance_table() -> None:
-    cur.execute(""" INSERT INTO sampled_trades_by_minute_binance  
+def insert_sampled_trades_by_minute_data_into_binance_table(sampled_by: str, table_name: str) -> None:
+    cur.execute(f""" INSERT INTO {table_name}  
                 SELECT symbol, timestamp, avg(price)
                 FROM "trades-BINANCE"
-                SAMPLE BY 1m;     
+                SAMPLE BY {sampled_by};     
                     """)
     
 
@@ -29,10 +26,11 @@ with pg.connect(conn_str, autocommit=True) as connection:
     # Open a cursor to perform database operations
     with connection.cursor() as cur:
         #Query the database and obtain data as Python objects.
-        create_sampled_trades_binance_table('sampeled_by_hour_trades')
-        records = cur.fetchall()
-        for row in records:
-            print(row)    
+       table_name: str = 'sampled_trades_by_hour'
+       sampled_by: str = '1h'
+       #create_sampled_trades_binance_table(smapled_table_name=table_name)
+       insert_sampled_trades_by_minute_data_into_binance_table(sampled_by=sampled_by, table_name=table_name)
+      
          
   
                 
