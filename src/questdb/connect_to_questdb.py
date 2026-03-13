17fa573f4c21263c928ffff9a04aa38fed1afc0c
ofekax <mtmn16@matmon-development-machine.xmeg0jdevaee3lh03vzpt13orh.tlvx.internal.cloudapp.net>
2026-03-13 09:51:43 +0000

    create sampled_trades_by_minute table and insert into it data

--- src/questdb/connect_to_questdb.py
+++ src/questdb/connect_to_questdb.py
@@ -11,29 +11,34 @@ def create_sampled_trades_binance_table(smapled_table_name: str) -> None:
                 DEDUP UPSERT KEYS(symbol, ts);
                     """)
 
-def insert_sampled_trades_by_minute_data_into_binance_table(sampled_by: str, table_name: str) -> None:
+def insert_sampled_trades_data_into_a_table(sampled_by: str, table_name: str) -> None:
     cur.execute(f""" INSERT INTO {table_name}  
                 SELECT symbol, timestamp, avg(price)
-                FROM "trades-BINANCE"
+                FROM "trades"
                 SAMPLE BY {sampled_by};     
                     """)
     
 
 conn_str = 'user=admin password=quest host=0.0.0.0 port=8812 dbname=trades-BINANCE'    
 
+sampled_by_hour_table_name: str = 'sampled_trades_by_hour'
+sampled_by_hour: str = '1h'
+
+sampled_by_minute_table_name: str = 'sampled_trades_by_minute'
+sampled_by_minute: str = '1m'
+
 
 with pg.connect(conn_str, autocommit=True) as connection:
     # Open a cursor to perform database operations
     with connection.cursor() as cur:
         #Query the database and obtain data as Python objects.
-       table_name: str = 'sampled_trades_by_hour'
-       sampled_by: str = '1h'
-       #create_sampled_trades_binance_table(smapled_table_name=table_name)
-       insert_sampled_trades_by_minute_data_into_binance_table(sampled_by=sampled_by, table_name=table_name)
-      
-         
-  
-                
-
-        
+       
+       #create_sampled_trades_binance_table(smapled_table_name=sampled_by_minute_table_name)
+       #insert_sampled_trades_data_into_a_table(sampled_by=sampled_by_minute, table_name=sampled_by_minute_table_name)
+       cur.execute(f"""SELECT * FROM {sampled_by_minute_table_name}  
+                    """)
+       records = cur.fetchall()
+       for row in records:
+         print(row)    
+                 
        
