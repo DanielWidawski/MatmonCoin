42f30539b6d90905640af14a456f817018776cac
ofekax <mtmn16@matmon-development-machine.xmeg0jdevaee3lh03vzpt13orh.tlvx.internal.cloudapp.net>
2026-03-14 16:58:43 +0000

    ...

--- src/questdb/connect_to_questdb.py
+++ src/questdb/connect_to_questdb.py
@@ -4,16 +4,16 @@ import time
 
     
 def create_binance_trades_materilized_view(materilized_view_name: str, sample_by: str) -> None:
-   cur.execute(f""" CREATE MATERIALIZED VIEW {materilized_view_name} REFRESH IMMEDIATE AS (
+   cur.execute(f""" CREATE MATERIALIZED VIEW {materilized_view_name} REFRESH EVERY 1m AS (
                     SELECT symbol, timestamp, avg(price) AS avg_price, min(price) AS min_price, max(price) AS max_price
-                    FROM "trades"
+                    FROM "temp"
                     SAMPLE BY {sample_by}
                ) PARTITION BY DAY TTL 7 DAYS
                     """)
    
 
 
-conn_str = 'user=admin password=quest host=0.0.0.0 port=8812 dbname=trades-BINANCE'    
+conn_str = 'user=admin password=quest host=0.0.0.0 port=8812 dbname=temp'    
 
 sampled_by_minute_materilized_view_name: str = 'binance_trades_minutely'
 sampled_by_minute: str = '1m'
@@ -21,15 +21,15 @@ sampled_by_minute: str = '1m'
 sampled_by_hour_materilized_view_name: str = 'binance_trades_hourly'
 sampled_by_hour: str = '1h'
 
-sampled_by_day_materilized_view_name: str = 'sampled_trades_by_daily'
+sampled_by_day_materilized_view_name: str = 'binance_trades_daily'
 sampled_by_day: str = '1d'
 
 
 
 with pg.connect(conn_str, autocommit=True) as connection:
      with connection.cursor() as cur:
-       #create_binance_trades_materilized_view(materilized_view_name=sampled_by_day_materilized_view_name, sample_by=sampled_by_day)
-       cur.execute(f"""SELECT * FROM {sampled_by_day_materilized_view_name}  
+       #create_binance_trades_materilized_view(materilized_view_name=sampled_by_minute_materilized_view_name, sample_by=sampled_by_minute)
+       cur.execute(f"""SELECT * FROM {sampled_by_minute_materilized_view_name}  
                     """)
        records = cur.fetchall()
        for row in records:
