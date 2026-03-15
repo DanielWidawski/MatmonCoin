504507da01ae8d5e19300b5497218509db772196
DanielWidawski <daniel.widawski06@gmail.com>
2026-03-15 14:15:37 +0000

    some design in db_writer, need abstractions

---
+++ src/db_writer/db_writer.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+import json
+
+from questdb.ingress import Sender
+
+
+def write_to_questdb(mech, method, properties, body):
+    msg = json.loads(body.decode('utf-8'))
+    conf = 'http::addr=localhost:9000;'
+    with Sender.from_conf(conf) as sender:
+        sender.row("trades",
+                   symbols={'symbol': msg.get('symbol'), 'market': msg.get('market').upper(), 'side': msg.get('side')},
+                   columns={'price': float(msg.get('price')), 'amount': float(msg.get('amount'))},
+                   at= datetime.datetime.fromtimestamp(msg.get('timestamp')))
--- src/db_writer/mq_reader.py
+++ src/db_writer/mq_reader.py
@@ -9,35 +9,24 @@ import datetime
 import pika
 from pika.exceptions import StreamLostError
 
+from src.db_writer.db_writer import write_to_questdb
 
-def main():
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-    channel = connection.channel()
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+channel.queue_declare(queue='ms')
 
-    channel.queue_declare(queue='ms')
-
-    """def callback(ch, method, properties, body):
-        print(f" [x] Received {json.loads(body.decode('utf-8'))}")"""
-
+def read_from_mq():
     channel.basic_consume(queue='ms', on_message_callback=write_to_questdb, auto_ack=True)
-
     print(' [*] Waiting for messages. To exit press CTRL+C')
     try:
         channel.start_consuming()
     except StreamLostError:
-        print(1)
-        main()
+        print("Stream lost, reconnecting...")
+        read_from_mq()
 
-
-def write_to_questdb(mech, method, properties, body):
-    msg = json.loads(body.decode('utf-8'))
-    conf = 'http::addr=localhost:9000;'
-    with Sender.from_conf(conf) as sender:
-        sender.row("trades",
-                   symbols={'symbol': msg.get('symbol'), 'market': msg.get('market').upper(), 'side': msg.get('side')},
-                   columns={'price': float(msg.get('price')), 'amount': float(msg.get('amount'))},
-                   at= datetime.datetime.fromtimestamp(msg.get('timestamp')))
-
+def main():
+    read_from_mq()
+    
 if __name__ == '__main__':
     try:
         main()
